e4b8ce58828486b4278ffde069c7e12f12009976
yaelBrown <yaelrbrown@gmail.com>
2022-05-02 22:23:13 -0500

    ran prettier

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,42 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
+import "./App.css";
 
-
-import './App.css';
-
-
-
-const links = require('./links.json')
-
+const links = require("./links.json");
 
 function App() {
-  const renderLinks = e => {
-    let out = []
+  const renderLinks = (e) => {
+    let out = [];
     Object.keys(links).map((e, i) => {
-      out.push(<a href={links[e]} key={i}>{e}</a>)
-    })
-    return out
-  }
-  
-  const [text, setText] = useState("")
-  
-  const handleChangeText = e => {
-    setText(e.target.value)
-  }
+      out.push(
+        <a href={links[e]} key={i}>
+          {e}
+        </a>
+      );
+    });
+    return out;
+  };
 
-  const copyText = () => navigator.clipboard.writeText(text)
+  const [text, setText] = useState("");
+
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const copyText = () => navigator.clipboard.writeText(text);
+  const clearText = () => setText("");
 
   return (
     <div className="App">
       <h1>App for Central grading</h1>
-      <hr/>
+      <hr />
 
       <section id="btm">
-        <div id="left">
-          {renderLinks()}
-        </div>
+        <div id="left">{renderLinks()}</div>
         <div id="right">
-          <textarea onChange={handleChangeText}>
-          </textarea>
+          <textarea onChange={handleChangeText} value={text} />
           <button onClick={copyText}>Copy</button>
+          <button onClick={clearText}>Clear</button>
         </div>
       </section>
     </div>
