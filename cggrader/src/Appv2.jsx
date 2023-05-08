e4c1c230ce1612b89ab2e45dbae0211fdb54b356
yaelBrown <yaelrbrown@gmail.com>
2023-05-08 10:25:19 -0500

    save: added mod9 feedback

--- cggrader/src/Appv2.jsx
+++ cggrader/src/Appv2.jsx
@@ -107,7 +107,7 @@ export default function Appv2() {
           <button onClick={() => addFeedbackFromJson(feedbackJSON.perfectMod3, mod3)} style={{backgroundColor: "#d3f6ff"}}>perfect-m3</button>
           <button onClick={() => addFeedbackFromJson(feedbackJSON.perfectMod4, mod4)} style={{backgroundColor: "#d3f6ff"}}>perfect-m4</button>
           <button onClick={() => addFeedbackFromJson(feedbackJSON.perfectMod5, mod5)} style={{backgroundColor: "#d3f6ff"}}>perfect-m5</button>
-          <button onClick={() => addFeedbackFromJson(feedbackJSON.init, mod9)} style={{backgroundColor: "#d3f6ff"}}>perfect-m9</button>
+          <button onClick={() => addFeedbackFromJson(feedbackJSON.perfectMod9, mod9)} style={{backgroundColor: "#d3f6ff"}}>perfect-m9</button>
           <button onClick={() => addFeedbackFromJson(feedbackJSON.perfectMod14, mod14)} style={{backgroundColor: "#d3f6ff"}}>perfect-m14</button>
           <button onClick={() => addFeedbackFromJson(feedbackJSON.perfectMod19, mod19)} style={{backgroundColor: "#d3f6ff"}}>perfect-m19</button>
           <hr/>
